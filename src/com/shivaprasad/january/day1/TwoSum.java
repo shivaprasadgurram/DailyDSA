c210de1fc5317a378b18a053a32413a58d7f0e53
Shiva Prasad Gurram <gurramshiva05@gmail.com>
2022-01-01 14:19:19 +0530

    leetcode #1

--- src/com/shivaprasad/january/day1/TwoSum.java
+++ src/com/shivaprasad/january/day1/TwoSum.java
@@ -1,9 +1,44 @@
 package com.shivaprasad.january.day1;
 
-public class TwoSum {
+import java.util.HashMap;
+import java.util.Map;
 
+public class TwoSum {
     public static void main(String[] args) {
 
+        int[] nums = {2,7,11,15};
+        int[] output = twoSum(nums,9);
+
+        if(output!=null)
+        {
+            for(int i : output)
+            {
+                System.out.println(i);
+            }
+        }
+        else
+        {
+            System.out.println("No match found!");
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+            int remaining = target - nums[i];
+            if(map.containsKey(remaining))
+            {
+                return new int[]{i,map.get(remaining)};
+            }
+            else
+            {
+                map.put(nums[i],i);
+            }
+        }
+
+        return null;
 
     }
 }
